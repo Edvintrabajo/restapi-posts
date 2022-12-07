f7f7ea2b93150baaa1d77ff932ad198a4ae88a1c
EdvinTrabajo <edvintrabajo@gmail.com>
2022-12-07 12:53:03 +0000

    config added

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_PORT = process.env.DB_PORT;
+export const DB_HOST = process.env.MYSQLHOST;
+export const DB_USER = process.env.MYSQLUSER;
+export const DB_PASSWORD = process.env.MYSQLPASSWORD;
+export const DB_DATABASE = process.env.MYSQLDATABASE;
+export const DB_PORT = process.env.MYSQLPORT;
