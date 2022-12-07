760ca846c0cbaebe3e308ad84c98c197dcdea0a8
EdvinTrabajo <edvintrabajo@gmail.com>
2022-12-07 12:27:14 +0000

    config added

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "edvin";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "ono99d2d2";
-export const DB_DATABASE = process.env.DB_DATABASE || "postsdb";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_HOST = "containers-us-west-160.railway.app";
+export const DB_USER = "root";
+export const DB_PASSWORD = "sTGkfFq3406bEEnvGkt5";
+export const DB_DATABASE = process.env.DB_DATABASE || "railway";
+export const DB_PORT = 5779;
