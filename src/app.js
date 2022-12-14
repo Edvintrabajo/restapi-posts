95510339ec8caa2658f06f4a5435e9b10427892d
edvintrabajo <edvintrabajo@gmail.com>
2022-12-14 18:53:38 +0100

    añadiendo cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 import postsRoutes from "./routes/posts.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 
@@ -7,6 +7,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/", indexRoutes);
